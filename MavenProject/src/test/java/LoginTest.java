30b6107d3ce3761ffd105856ee212403e6016a71
Lalita Pant <lalita.asm@gmail.com>
2020-02-19 13:13:53 -0500

    First Commit

---
+++ MavenProject/src/test/java/LoginTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	@BeforeTest
+	public void setup()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+		
+		
+	}
+	@Test
+	public void doLogin()
+	{
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("lalita.asm");
+		driver.findElement(By.id("Passwd")).sendKeys("lalita.asm");
+	}
+}
