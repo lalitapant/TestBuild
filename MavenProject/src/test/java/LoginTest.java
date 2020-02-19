f9c172415dd31cb541dcd56a10c01d01f9661c7c
Lalita Pant <lalita.asm@gmail.com>
2020-02-19 13:26:03 -0500

    third commit

--- MavenProject/src/test/java/LoginTest.java
+++ MavenProject/src/test/java/LoginTest.java
@@ -12,7 +12,7 @@ public class LoginTest {
 	@BeforeTest
 	public void setup()
 	{
-		//Login Test new
+		//Login Test new file
 		WebDriverManager.firefoxdriver().setup();
 		driver=new FirefoxDriver();
 		
